ebd8feb01a41d2cb7009a1959d03d74e516a34a9
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2024-12-23 11:34:37 +0900

    MySQLインストール+モデルのNull警告など解説追記

--- SelfAspNet/Models/MyContext.cs
+++ SelfAspNet/Models/MyContext.cs
@@ -5,7 +5,14 @@ using Microsoft.EntityFrameworkCore;
 //自分のモデル名にEntityNameを書き換えただけ(今回はSmaple)
 namespace SelfAspNet.Models {
     public class MyContext : DbContext {
-        public MyContext (DbContextOptions<MyContext> options) : base (options) { }
-        public DbSet<Sample> Samples { get; set; }
+        //
+        public MyContext (DbContextOptions<MyContext> options) : base (options) {
+            // requiredは初期化を補償してnullじゃないことを示すので、一見コンストラクタで初期化+代入が必要に見える
+            // が、DbContextが自動で初期化してくれるので不要になる
+            // Samples = new DbSet<Sample>(); // 不要な初期化
+         }
+
+        // SampleモデルをDbsetしてSmaplesメソッドで操作出来るようにしている
+        public required DbSet<Sample> Samples { get; set; }
     }
 }
