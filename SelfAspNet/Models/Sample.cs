0f81c820072f4281231bd9c551b5ab721c2125cb
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2024-12-24 17:10:32 +0900

    モデルをビューで参照する DisplayNameFor についてのメモ

--- SelfAspNet/Models/Sample.cs
+++ SelfAspNet/Models/Sample.cs
@@ -1,10 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace SelfAspNet.Models;
 
 public class Sample
 {
     public int id { get; set; }
+    // ビューの@Html.DisplayNameForで表示する文字をここでも設定できる
+    [Display(Name = "タイトル")]
     public string title { get; set; } = String.Empty;//初期値空文字
     public string sub_title { get; set; } = "";//初期値空文字はこれでもいいらしい
 }
