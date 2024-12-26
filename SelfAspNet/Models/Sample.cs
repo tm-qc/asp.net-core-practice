ac716c6e5b636efe79c82c1bb36f26089032a938
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2024-12-26 12:52:56 +0900

    modelで表示を整形する方法 結構使えるかも

--- SelfAspNet/Models/Sample.cs
+++ SelfAspNet/Models/Sample.cs
@@ -11,4 +11,15 @@ public class Sample
     [Display(Name = "タイトル")]
     public string title { get; set; } = String.Empty;//初期値空文字
     public string sub_title { get; set; } = "";//初期値空文字はこれでもいいらしい
+
+    // modelで表示を整形する方法
+     
+    // 今回金額のカラムないのでサンプル
+    // DataTypeで型を割り当てたら表示を自動で型に沿った表示にも出来る
+    // 金額のカラムなので、[DataType(DataType.Currency)]するとこの値を自動で1000を￥1,000に変換して表示してくれる
+
+    // 表示整形をがんばるくらいなら、この方法で統一して書くといいかも
+
+    // [DataType(DataType.Currency)]
+    // public string price { get; set; } = 0;
 }
