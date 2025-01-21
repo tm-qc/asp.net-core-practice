026b6844ee5279c2a869a328b4a4befb52ba2639
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2025-01-21 16:43:14 +0900

    標準サービスの注入について
    これでPJ作成した場合に
    dotnet new mvc -o ProjectName -f net8.0
    
    Program.csでアプリで使用するサービスを定義できる。
    初期はRazorなしのMVCのみで
    builder.Services.AddControllersWithViews();
    になってる

--- CoreIdentity/Program.cs
+++ CoreIdentity/Program.cs
@@ -14,6 +14,31 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddRoles<IdentityRole>()//ロール機能を有効化
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// ■標準サービスの注入について
+// builder.Services.AddXXX();で使うサービスを定義しビルド時に注入する
+// ここを書き換えた後は再ビルドで適用される
+// 
+// AddControllersWithViews()は以下を含む
+// 
+// ・AddControllersWithViews：MVCアプリ
+// ・AddControllers：WebAPI機能
+// ・AddMvcCore:MVC基幹機能
+// 
+// ▼Razor Pages をメインに利用したい場合
+// AddControllersWithViewsには含まれないので以下に変更が必要
+// この場合AddControllersWithViews(コントローラーなど)は動かなくなる
+// 
+// builder.Services.AddRazorPages()
+// 
+// ▼AddControllersWithViewsとAddRazorPagesの併用
+// 
+// AddRazorPages
+// なお、builder.Services.AddControllersWithViews();を定義したまま
+// app.MapRazorPages();を定義すればRazorとMVCの併用ができる
+// 
+// もしくはそもそも全部入りのサービスを注入してもいい
+// builder.Services.AddMvc();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
